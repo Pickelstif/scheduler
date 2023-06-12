27df0cce8ee4f81f88a6982dc4e1fc558d01b4cc
Gregory Bell <ghbell@gmail.com>
2023-06-12 16:14:42 +0800

    after changing to all bandmates on same page

--- website/__init__.py
+++ website/__init__.py
@@ -31,6 +31,8 @@ def create_app():
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
+    # one commit
+
     @login_manager.user_loader
     def load_user(id):
         user = User.query.get(int(id))
